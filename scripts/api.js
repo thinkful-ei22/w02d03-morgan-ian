cf02cace4f49351923d2ca1c454c205800e716b4
MorganHuegel <mhue2910@gmail.com>
2018-06-20 14:49:52 -0400

    successfully pushes items to server and Store when Submit button is clicked

---
+++ scripts/api.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const api = (function(){
+  const BASE_URL = "https://thinkful-list-api.herokuapp.com/ian";
+  
+  const getItems = function(callback){
+    $.getJSON(`${BASE_URL}/items`, callback);
+  };
+
+
+  const createItem = function(name, callback){
+    let newItem = JSON.stringify({ name: name,});
+    $.ajax({
+      url: `${BASE_URL}/items`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: newItem,
+      success: callback
+    });
+  };
+
+
+  return {
+    getItems, createItem
+  };
+}());
